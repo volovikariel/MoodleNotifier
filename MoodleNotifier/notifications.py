18005dbe6be64ecff34505ba3d27a7cb24d29e5d
volovikariel <16262535+volovikariel@users.noreply.github.com>
2022-01-05 19:38:25 -0500

    We now pass in more parameters (email credentials, subject, course_link, course_name)

--- MoodleNotifier/notifications.py
+++ MoodleNotifier/notifications.py
@@ -1,7 +1,10 @@
 import smtplib
-
 from email.message import EmailMessage
-from datetime import datetime
+from typing import List
+
+from moodle_notifier import get_formatted_time
+from credentials import EmailCredentials
+
 
 # How to get an app password (link: https://support.google.com/mail/answer/185833?hl=en-GB)
 # 1. go to manage my google account (link: https://myaccount.google.com/security)
@@ -9,28 +12,19 @@ from datetime import datetime
 # 3. Under "Signing in to Google" Select "App passwords".
 # 4. Select the app as "Mail" and the device as "Other (Custom name)" and name it (e.g: moodle_notifications).
 # 5. Copy the app password, it will be in a yellow box and looks like: "XXXX XXXX XXXX XXXX"
-def send_email(content: str, email_from: str, email_to: str, app_password: str) -> None:
-    """We send an email from email_from to email-to with the provided content through an SMTP server using GMAIL as the provider"""
-
+def notify_email(subject: str, content_body: str, email_credentials: EmailCredentials, email_to: List[str], course_link: str, course_name: str) -> None:
+    """We send an email from the user to the email_to list, with the provided subject and content, through an SMTP server (using GMAIL as the provider)"""
     msg = EmailMessage()
-    msg.add_header("Content-Type","text/html")
-    msg.set_content(content, subtype="HTML")
 
-    msg["Subject"] = f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] New Moodle Post"
-    msg["From"]    = email_from
+    content_header = f"<header>Retrieved from the <a href={course_link}>{course_name}</a> course page</header>"
+    content_footer = f"<footer>Sent at: <b>{get_formatted_time()}</b></footer>"
+    msg.set_content(content_header + content_body + content_footer, subtype="HTML")
+
+    msg["Subject"] = subject
+    msg["From"]    = email_credentials.email
     msg["To"]      = email_to
 
     with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
-        server.login(email_from, app_password)
+        server.login(email_credentials.email, email_credentials.app_password)
         server.send_message(msg)
-
-if __name__ == "__main__":
-    """We only ever run this if we're testing the notification functions"""
-    import os
-    from dotenv import load_dotenv
-    # Storing the APP_PASSWORD in a .env file so we need to load it in
-    load_dotenv()
-    email        = os.environ.get("EMAIL")
-    app_password = os.environ.get("APP_PASSWORD")
-    send_email("<b>Test content</b>", email, email, app_password)
