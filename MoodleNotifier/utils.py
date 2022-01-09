792aa82e094561bbd1ebea3eebe8dc9ddb5022b3
volovikariel <16262535+volovikariel@users.noreply.github.com>
2022-01-09 13:00:38 -0500

    We now create a default environment file if the user doesn't have one yet

--- MoodleNotifier/utils.py
+++ MoodleNotifier/utils.py
@@ -6,3 +6,30 @@ def get_formatted_time() -> str:
 
 def file_exists(file_path: str) -> bool:
     return os.path.isfile(file_path)
+
+def create_default_env_file() -> None:
+    from textwrap import dedent
+    if not file_exists(".env"): 
+        file_content = dedent("""\
+                                #Your concordia portal username
+                                USERNAME="concordia.ca\\\\example"
+                                #Your Concordia portal password
+                                PASSWORD=""
+                                #Your gmail email
+                                EMAIL="@gmail.com"
+                                #The gmail APP password
+                                # How to get an app password (instructions tkaen from: https://support.google.com/mail/answer/185833?hl=en-GB)
+                                # 1. go to manage my google account (link: https://myaccount.google.com/security)
+                                # 2. Under "Signing in to Google" confirm that "2-Step Verification" is "On" for the account.
+                                # 3. Under "Signing in to Google" Select "App passwords".
+                                # 4. Select the app as "Mail" and the device as "Other (Custom name)" and name it (e.g: moodle_notifications).
+                                # 5. Copy the app password, it will be in a yellow box and looks like: "XXXX XXXX XXXX XXXX"
+                                APP_PASSWORD=""
+                                #The list of recipients that will be notified of posts/deletions
+                                EMAIL_RECIPIENTS='["@gmail.com"]'
+                                #Delay in hours, minutes, and seconds
+                                DELAY_HOURS=0
+                                DELAY_MINUTES=5
+                                DELAY_SECONDS=0""")
+        with open(".env", "w+") as env_file:
+            env_file.write(file_content)
